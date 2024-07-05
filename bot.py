97012c00b2174e178bd9d1971dbd34e5e33f4212
dannniellol <danielwai423@gmail.com>
2024-07-06 00:01:08 +0800

    try this function

--- bot.py
+++ bot.py
@@ -5,15 +5,37 @@ import os
 import discord
 from dotenv import load_dotenv
 
+# load_dotenv()
+# TOKEN = os.getenv('DISCORD_TOKEN')
+# GUILD = os.getenv('DISCORD_GUILD')
+
+# intents = discord.Intents.default()
+# client = discord.Client(intents=intents)
+
+# @client.event
+# async def on_ready():
+#     print(f'{client.user} has connected to Discord!')
+
+# client.run(TOKEN)
+
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
-intents = discord.Intents.default()
-client = discord.Client(intents=intents)
+client = discord.Client()
 
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
+
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})\n'
+    )
+
+    members = '\n - '.join([member.name for member in guild.members])
+    print(f'Guild Members:\n - {members}')
 
 client.run(TOKEN)
