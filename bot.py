8aee547000c8b730c0a9a81db827ae240a628708
Gabriel Leung <gabby.t.leung@gmail.com>
2024-07-06 16:12:59 +0800

    minor fixes

--- bot.py
+++ bot.py
@@ -1,5 +1,8 @@
 """
+Main script for the bot
 
+Credits:
+adapted from https://realpython.com/how-to-make-a-discord-bot-python/
 """
 from func import *
 
@@ -17,29 +20,30 @@ async def on_ready():
     """
     Starts up the bot, creates a connection with target server,
     then prints out the server's name and id.
-
-    Credits:
-    adapted from https://realpython.com/how-to-make-a-discord-bot-python/
     """
-    init(c)
+    guild = discord.utils.get(c.guilds, name=GUILD)
+    print(f"{c.user} is connected to the following guild:\n"
+          f"{guild.name}(id: {guild.id})\n"
+          f"Time: {datetime.now().strftime('%H:%M:%S')}")
 
 
 @c.event
-async def on_message(message):
+async def on_message(message: discord.Message):
     """
+    Executes upon receiving a message in any Discord channel the bot has access
+    to. Searches for `c.cmd_key` and runs relevant commands.
+    """
+    msg = msg_split(message.content)
 
-    """
-    # print(message.content, c.cmd_key)
-    msg = message.content.split(" ")
+    # bot should not react to its own messages
     if message.author == c.user:
         return
+
     if msg[0] == c.cmd_key:
         try:
             await command(c, msg, message.channel)
         except ValueError as e:
-            await send_msg(c,
-                           f"Error: {e.args[0]}",
-                           message.channel)
+            await send_msg(c, f"Error: {e.args[0]}", message.channel)
 
 
 c.run(TOKEN)
